5fc071f1ce11d4e1469e41affa29c014819bb9a0
Hien <hienb697@gmail.com>
2022-07-05 15:41:31 +0700

    Add project files.

---
+++ Client/Client/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 100;
+
+        static void Main()
+        {
+            Console.Title = "Client";
+            ConnectToServer();
+            RequestLoop();
+            Exit();
+        }
+
+        private static void ConnectToServer()
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine("Connection attempt " + attempts);
+                    // Change IPAddress.Loopback to a remote IP to connect to a remote host.
+                    ClientSocket.Connect(IPAddress.Loopback, PORT);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
+
+        private static void RequestLoop()
+        {
+            Console.WriteLine(@"<Type ""exit"" to properly disconnect client>");
+
+            while (true)
+            {
+                SendRequest();
+                ReceiveResponse();
+            }
+        }
+
+        /// <summary>
+        /// Close socket and exit program.
+        /// </summary>
+        private static void Exit()
+        {
+            SendString("exit"); // Tell the server we are exiting
+            ClientSocket.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        private static void SendRequest()
+        {
+            Console.Write("Send a request: ");
+            string request = Console.ReadLine();
+
+
+            if (request.ToLower() == "exit")
+            {
+                Exit();
+            }else if(request.ToLower() == "login")
+            {
+                LoginForm();
+            }    
+        }
+
+        /// <summary>
+        /// Sends a string to the server with ASCII encoding.
+        /// </summary>
+        private static void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void LoginForm()
+        {
+            Console.WriteLine("Enter your user name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your password: ");
+            string password = Console.ReadLine();
+            
+            SendLoginData(name, password);
+        } 
+
+        private static void SendLoginData(string name,string password)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes("login"+ " " + name + " " + password);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void SendResigterData(string name,string password,string creditNumber)
+        {
+
+        }
+        /// <summary>
+        /// end modifiler for socket
+        /// </summary>
+        private static void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ server/server/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        private static byte[] _buffer = new byte[1024];
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            SetupServer();
+
+            Console.ReadLine();
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Starting Server");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any,100));
+            _serverSocket.Listen(5);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+
+        }
+
+        private static void AcceptCallBack(IAsyncResult AR)
+        {
+            Socket socket = _serverSocket.EndAccept(AR);
+            _clientSockets.Add(socket);
+            Console.WriteLine("Client Connected to Server");
+            socket.BeginReceive(_buffer,0,_buffer.Length,SocketFlags.None, new AsyncCallback(ReceiveCallback),socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] dataBuf = new byte[received];
+            Array.Copy(_buffer,dataBuf,received);
+            string text = Encoding.ASCII.GetString(dataBuf);
+
+            Console.WriteLine("Text received : " + text);
+
+            string response = string.Empty;
+            String[] strs = ReadData(text);
+
+            if (strs[0].ToLower() != "login")
+            {
+                response = "Invalid Request";
+            }
+            else
+            {  
+                if (strs[1] == "MinhHien" && strs[2] == "123456")
+                {
+                    response = "Login Success";
+                }else
+                {
+                    response = "Wrong username or password";
+                }
+            }
+
+            byte[] data = Encoding.ASCII.GetBytes(response);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+
+        }
+        /// <summary>
+        /// modifile function write here
+        /// </summary>
+        private static string[] ReadData(string text)
+        {
+           String[] splitStr = text.Split(" ");
+
+            return splitStr;
+        }
+
+        private static void GetInput(string text, IAsyncResult AR)
+        {
+            switch(text)
+            {
+                case "login":
+                    break;
+                case "signup":
+                    break;
+                case "logout":
+                    break;
+                case "checkout":
+                    break;
+            }
+        }
+
+        private static void Register(IAsyncResult AR)
+        {
+            
+        }
+
+        private static void Login(IAsyncResult AR)
+        {
+
+        }
+
+        private static void CheckOut(IAsyncResult AR)
+        {
+
+        }
+
+        private static void Booking(IAsyncResult AR)
+        {
+
+        }
+
+        /// <summary>
+        /// End Modified function
+        /// </summary>
+
+        private static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+
+            socket.EndSend(AR);
+        }
+    }
+
+    class Customer{
+        public string userName;
+        public string password;
+        public string creditNumber;
+
+        public Customer(string userName, string password,string number)
+        {
+            this.userName = userName;
+            this.password = password;
+            this.creditNumber = number;
+        }
+
+    }
+}
